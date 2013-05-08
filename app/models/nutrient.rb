e005c7ff988768a3f2f8b268f65c0c2c67ba8583
adamwong246 <adam@spaceport.io>
2013-05-08 00:36:34 -0700

    added footer styling fixed nutrient association extracted super_form_errors

--- app/models/nutrient.rb
+++ app/models/nutrient.rb
@@ -6,14 +6,14 @@ class Nutrient < ActiveRecord::Base
   has_many :component_nutrients,                                autosave: true
   has_many :components,          through: :component_nutrients, autosave: true
   has_many :ingredients,         through: :components,          autosave: true
-  has_many :recipes,             through: :ingredients,         autosave: true
+  # has_many :recipes,             through: :ingredients,         autosave: true
 
   attr_accessible :id, :name, :fda_rda
 
   accepts_nested_attributes_for :component_nutrients
   accepts_nested_attributes_for :components
   accepts_nested_attributes_for :ingredients
-  accepts_nested_attributes_for :recipes
+  # accepts_nested_attributes_for :recipes
 
   def self.identify
     "A Nutrient is the FDA recommended amount of a certain substance."
